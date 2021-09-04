17f27176a99adbca8d28e044897bd65bc47c29ad
itzhakstern <itzhak7915@gmail.com>
2021-09-05 00:29:18 +0300

    1.1 fix the page factory idea

--- create_xml_config.py
+++ create_xml_config.py
@@ -1,15 +1,4 @@
 from lxml import etree as et
-import random
-
-browser_options = ["Chrome", "Edge"]
-first_name_options = ["Itzhak", "Jek", "Sami", "Josh"]
-
-INPUT = {"browser_type": random.choice(browser_options),
-         "excepted_title": "BUYME אתר המתנות והחוויות הגדול בישראל |\xa0Gift Card",
-         "first_name": random.choice(first_name_options),
-         'mail': f"{random.randint(1111111111,99999999999)}@gmail.com",
-         "password": "Abcd1234",
-         "password_confirm": "Abcd1234"}
 
 
 class CreateConfig:
@@ -23,11 +12,3 @@ class CreateConfig:
 
     def write_to_file(self, file_name):
         self.__tree.write(file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
-
-
-def create_xml_file():
-    config = CreateConfig("buy_me_website")
-    for key, value in INPUT.items():
-        config.add_child(key, INPUT[key])
-    config.write_to_file("config_xml_for_buyme.xml")
-
--- test_buyme.py
+++ test_buyme.py
@@ -1,36 +1,83 @@
 import unittest
 from selenium.common.exceptions import NoSuchElementException
 import sys
+from create_xml_config import CreateConfig
+import random
+from lxml import etree as et
 import page_factory_for_buyme_test
+from selenium import webdriver
 
 
-class MyTestCase(unittest.TestCase):
+class ByuMeTest(unittest.TestCase):
+    driver = None
+    page_factory = None
+    DATA = {}
+
     @classmethod
     def setUpClass(cls) -> None:
-        cls.page_factory = page_factory_for_buyme_test.PageFactory()
-        cls.page_factory.init_driver()
-        cls.driver = cls.page_factory.driver
-        cls.page_factory.open_buyme_website()
+        cls.driver = ByuMeTest.init_driver()
+        cls.page_factory = page_factory_for_buyme_test.PageFactory(cls.driver)
+
+    @staticmethod
+    def init_driver():
+        driver = None
+        if ByuMeTest.DATA['browser_type'] == 'Chrome':
+            driver = webdriver.Chrome()
+        elif ByuMeTest.DATA['browser_type'] == 'Edge':
+            driver = webdriver.Edge()
+        buyme_url = 'https://buyme.co.il/'
+        driver.maximize_window()
+        driver.get(buyme_url)
+        return driver
 
     def test_regist_to_buyme(self):
-        self.page_factory.insert_name()
-        self.page_factory.insert_email()
-        self.page_factory.insert_password()
-        self.page_factory.insert_confirm_password()
+        self.page_factory.insert_name(ByuMeTest.DATA["first_name"])
+        self.page_factory.insert_email(ByuMeTest.DATA["mail"])
+        self.page_factory.insert_password(ByuMeTest.DATA["password"])
+        self.page_factory.insert_confirm_password(ByuMeTest.DATA["password_confirm"])
         self.page_factory.registration()
 
+        # check if the botton 'החשבון שלי' is exists
         try:
             self.driver.find_element_by_id("ember1530")
             flag = True
         except NoSuchElementException:
             flag = False
-        self.assertTrue(flag, "attempt to register to the 'buyme' site id filed")
-        self.assertEqual(self.driver.title, self.page_factory.DATA['excepted_title'])
+        self.assertTrue(flag, "attempt to register to the 'buyMe' site id filed")
+        self.assertEqual(self.driver.title, ByuMeTest.DATA['excepted_title'])
+
+
+#####------------------- main function --------------------####
+
+browser_options = ["Chrome", "Edge"]
+first_name_options = ["Itzhak", "Jek", "Sami", "Josh"]
+
+INPUT = {"browser_type": random.choice(browser_options),
+         "excepted_title": "BUYME אתר המתנות והחוויות הגדול בישראל |\xa0Gift Card",
+         "first_name": random.choice(first_name_options),
+         'mail': f"{random.randint(1111111111, 99999999999)}@gmail.com",
+         "password": "Abcd1234",
+         "password_confirm": "Abcd1234"}
+
+
+def create_xml_file():
+    config = CreateConfig("buy_me_website")
+    for key, value in INPUT.items():
+        config.add_child(key, INPUT[key])
+    config.write_to_file("config_xml_for_buyme.xml")
+
+
+def data_from_xml_config():
+    contents_xml_file = et.parse('config_xml_for_buyme.xml')
+    for items in contents_xml_file.iter():
+        ByuMeTest.DATA[items.tag] = items.text
 
 
 def main(out):
-    loader = unittest.TestLoader()
+    create_xml_file()
+    data_from_xml_config()
 
+    loader = unittest.TestLoader()
     suite = loader.loadTestsFromModule(sys.modules[__name__])
     unittest.TextTestRunner(out).run(suite)
 
